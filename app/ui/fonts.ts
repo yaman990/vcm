de2d1b0142bfd0647c556b5672c4433c21315fcf
Yaman Almasri <yaman.almasri01@gmail.com>
2024-03-26 16:18:52 +0300

    hello

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import {Inter , Lusitana} from 'next/font/google';
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+})
+export const inter = Inter({subsets:['latin']})
